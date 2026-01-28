84c230b4c69e66273f56cbbc60d0a79f68f9921f
BriseDeFeuilles <brisedefeuilles@gmail.com>
2026-01-28 22:09:45 +0100

    Update env.py

--- env.py
+++ env.py
@@ -2,19 +2,20 @@
 
 #import
 import sys 
-import time
-import sysv_ipc
+from time import sleep
+#import sysv_ipc
 import multiprocessing as mp
 import socket
 import select
 import signal
 import threading
 import prey
+import predator
 from multiprocessing import Process, Value
 from multiprocessing.managers import BaseManager
 import random
 from class_data import Data
-from Fct_mq_display_grass import stop_display, secheresse, incGrass, msg_display
+from Fct_mq_display_grass import stop_display, secheresse, incGrass, msg_display, msg_display_no_mq
 
 #constantes
 HOST = "localhost"
@@ -37,7 +38,7 @@ lock_grass = mp.Lock()
 class MyManager(BaseManager):
     pass
 
-MyManager.register('get_Data', callable=lambda: data, exposed=['add_prey', 'get_prey', 'get_predator', 'get_grass', 'kill_prey', 'kill_predator', 'add_grass', 'eat_grass', 'active_prey', 'passive_prey'])
+MyManager.register('get_Data', callable=lambda: data, exposed=['add_prey', 'add_predator', 'get_prey', 'get_predator', 'get_grass', 'kill_prey', 'kill_predator', 'add_grass', 'eat_grass', 'eat_prey', 'active_prey', 'passive_prey', 'active_predator', 'passive_predator', 'kill_all'])
 MyManager.register("get_lock_prey", callable=lambda: lock_prey)
 MyManager.register("get_lock_predator", callable=lambda: lock_predator)
 MyManager.register("get_lock_grass", callable=lambda: lock_grass)
@@ -57,21 +58,22 @@ def handler_signal(signum, frame) :
 #process principale
 def env():
     manager = MyManager(address=("localhost", PORT_M), authkey=b"clef")
-    print(MyManager._registry['get_Data'])
     while True:
         try:
             manager.connect()
             break
         except ConnectionRefusedError:
-            time.sleep(0.1)
-    print("connect manager", flush=True)
+            sleep(0.1)
 
     ### Flag utiles pour le display et l'herbe
     flag_display = Value('b', True)
     flag_grass = Value('b', True)
-    signal.signal(signal.SIGUSR1, handler_signal)
+    if sys.platform != "win32":
+        signal.signal(signal.SIGUSR1, handler_signal)
+    else:
+        print("SIGUSR1 non disponible")
 
-    mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
+    #mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
 
     data = manager.get_Data() 
     lock_prey = manager.get_lock_prey()
@@ -79,65 +81,87 @@ def env():
     lock_grass = manager.get_lock_grass()
     
 
-    print("serveur init", flush=True)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.setblocking(False)
 
-    thread_display = threading.Thread(target=msg_display, args=(mq, pop_prey, pop_predator, grass, flag_display,))      #ajouter les locks ...
-    thread_grass = threading.Thread(target=incGrass, args=(grass, flag_display, flag_grass,))
+    #thread_display = threading.Thread(target=msg_display, args=(mq, data, lock_prey, lock_predator, lock_grass, flag_display,))      #ajouter les locks ...
+    thread_display_no_mq = threading.Thread(target=msg_display_no_mq, args=(data, lock_prey, lock_predator, lock_grass, flag_display,))
+    thread_grass = threading.Thread(target=incGrass, args=(data, lock_grass, flag_display, flag_grass,))
     thread_drought = threading.Thread(target=secheresse, args=(drought_event, flag_grass, flag_display,))
-    thread_connection = threading.Thread(target=gestion_connection, args=(data, server_socket, True))
+    thread_connection = threading.Thread(target=gestion_connection, args=(data, server_socket, flag_display, lock_prey, lock_predator))
 
     thread_connection.start()
-    thread_display.start()
+    #thread_display.start()
+    thread_display_no_mq.start()
     thread_grass.start()
     thread_drought.start()
 
-    print("serveur start", flush=True)
-    for i in range(15):
-        time.sleep(1)
-        print("env : ", data.get_prey(), i, flush=True)
     
+    sleep(10)
+
+    ### makes sure that there are animals of both types, 
+    ### prepare to finish the simulation otherwise via flag_display
+    while flag_display.value == True :
+        sleep(3)
+        lock_prey.acquire()
+        if data.get_prey() == {} :
+            flag_display.value = False
+        lock_prey.release()
+        lock_predator.acquire()
+        if data.get_predator() == {} :
+            flag_display.value = False
+        lock_predator.release()
+            
+    print("flag = False")
+
+    ### kill all animal process
+    lock_predator.acquire()
+    lock_prey.acquire()
+    data.kill_all()
+    lock_predator.release()
+    lock_prey.release()
+
+    ### wait for all thread to finish
     thread_connection.join()
-    # Ces deux lignes sont à enlever, il faudra mettre une condition quelque part pour mettre ce flag à False
-    sleep(10) 
-    flag_display.value = False
-    
     thread_grass.join()
     thread_drought.join()
-    thread_display.join()
+    #thread_display.join()
+    thread_display_no_mq.join()
 
-    stop_display(mq)
+    print("every thread stop")
+
+    #stop_display(mq)
 
 
 
 #fonction de client/serveur
-def gestion_connection(data, server_socket, on, number=0):
+def gestion_connection(data, server_socket, flag_display, lock_prey, lock_predator):
     with server_socket:
         server_socket.listen(1)
-        while on == True :
+        while flag_display.value :
             readable, writable, error = select.select([server_socket], [], [], 1)
             if server_socket in readable:
                 client_socket, address = server_socket.accept()
-                thread_client = threading.Thread(target=gestion_clients, args=(data, client_socket, address))
+                thread_client = threading.Thread(target=gestion_clients, args=(data, client_socket, address, lock_prey, lock_predator))
                 thread_client.start()
                 thread_client.join()
-                print("connection : ", data.get_prey(), flush = True)
                 
 
 
-def gestion_clients(data, client_socket, address):
+def gestion_clients(data, client_socket, address, lock_prey, lock_predator):
     msg = client_socket.recv(64).decode()
     print(msg)
     pid, type, event = msg.split(",")
     if event == "new" :
         if type == "prey" :
-            print(pid)
+            lock_prey.acquire()
             data.add_prey(pid)
-            print("gestion", data.get_prey())
+            lock_prey.release()
         elif type == "predator" :
+            lock_predator.acquire()
             data.add_predator(pid)
+            lock_predator.release()
         
 
 #fonction principale
@@ -147,19 +171,18 @@ if __name__ == "__main__":
     p_memory = mp.Process(target=shared_memory)
     p_memory.start()
     
-    print("starting manager process")
-    
-    time.sleep(5)
+    sleep(2)
 
-    print("starting env process")
     p_env = mp.Process(target=env)
     p_env.start()
-    #fin = input()
-    time.sleep(5)
+
+    sleep(2)
     prey.main()
-"""    prey.main()
     prey.main()
-    time.sleep(1)
     prey.main()
-    prey.main()"""
+    prey.main()
+    prey.main()
+    predator.main()
+    predator.main()
+
 
