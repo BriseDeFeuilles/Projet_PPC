588069ffa608d5cbcb4941f6aa6bf62635a2f1f5
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-29 11:08:55 +0100

    Update env.py

--- env.py
+++ env.py
@@ -3,7 +3,7 @@
 ### Imports
 import sys 
 from time import sleep
-#import sysv_ipc
+import sysv_ipc
 import multiprocessing as mp
 import socket
 import select
@@ -11,17 +11,19 @@ import signal
 import threading
 import prey
 import predator
+import display
 from multiprocessing import Process, Value
 from multiprocessing.managers import BaseManager
 import random
 from class_data import Data
 from Fct_mq_display_grass import stop_display, secheresse, incGrass, msg_display, msg_display_no_mq
 
+
 ### Constantes de connections qui doivent être égale dans tous les programmes
 HOST = "localhost"
 PORT = 6666
 
-PORT_M = 50000
+PORT_M = 50001
 
 
 key = 777
@@ -56,31 +58,67 @@ def handler_signal(signum, frame) :
     drought_event.set()
 
 
-### Process principale
+### Fonction de gestion client/serveur
+def gestion_connection(data, server_socket, flag_display, lock_prey, lock_predator):
+    """
+    Fonction qui accepte les connections de tous les nouveaux animaux et crée une thread qui gère la connection
+    Paramètres :
+        data: Permet l'accès à la shared memory et à ses fonctions
+        server_socket: socket serveur à laquelle se connecte les nouveaux animaux
+        flag_display: flag qui annonce la fin du programme
+        lock_prey: lock d'accès pour lire et modifier pop_prey
+        lock_predator: lock d'accès pour lire et modifier pop_predator
+    """
+    with server_socket:
+        server_socket.listen(1)
+        while flag_display.value :
+            readable, writable, error = select.select([server_socket], [], [], 1)
+            if server_socket in readable:
+                client_socket, address = server_socket.accept()
+                thread_client = threading.Thread(target=gestion_clients, args=(data, client_socket, address, lock_prey, lock_predator))
+                thread_client.start()
+                thread_client.join()
+                
+
+# Fonction de gestion de la connection qui reçoit un message du nouvel animal et l'ajoute à la liste
+def gestion_clients(data, client_socket, address, lock_prey, lock_predator):
+    msg = client_socket.recv(64).decode()
+    print(msg)
+    pid, type, event = msg.split(",")
+    if event == "new" :
+        if type == "prey" :
+            lock_prey.acquire()
+            data.add_prey(pid)
+            lock_prey.release()
+        elif type == "predator" :
+            lock_predator.acquire()
+            data.add_predator(pid)
+            lock_predator.release()
+
+
+
+### Process principal
 def env():
     ### Connection au storage manager
     manager = MyManager(address=("localhost", PORT_M), authkey=b"clef")
-    while True: # Cette boucle s'assure que la connection au lieu
+    while True: # Cette boucle s'assure que la connection a lieu
         try:
             manager.connect()
             break
         except ConnectionRefusedError:
             sleep(0.1)
 
-    ### Flag utiles pour le display et l'herbe
+    ### Flags utiles pour le display et l'herbe
     flag_display = Value('b', True)
     flag_grass = Value('b', True)
 
-    ### Création du signal pour la secheresse, SIGUSR1 ne fonctionne pas sur toutes les machines 
-    if sys.platform != "win32":
-        signal.signal(signal.SIGUSR1, handler_signal)
-    else:
-        print("SIGUSR1 non disponible")
+    ### Création du handler_signal pour la sécheresse
+    signal.signal(signal.SIGUSR1, handler_signal)
 
     ### Création de la message queue vers le display
-    #mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
+    mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
 
-    ### Récupération dans le process des accès aux fonction et des locks de shared memory
+    ### Récupération dans le process des accès aux fonctions et des locks de la shared memory
     data = manager.get_Data() 
     lock_prey = manager.get_lock_prey()
     lock_predator = manager.get_lock_predator()
@@ -88,24 +126,23 @@ def env():
     
     ### Initialisation du serveur pour la connection des process animaux
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.setblocking(False)
 
     ### Création des threads de gestion de différents éléments et evenement
 
     # Thread d'affichage
-    #thread_display = threading.Thread(target=msg_display, args=(mq, data, lock_prey, lock_predator, lock_grass, flag_display,))      #ajouter les locks ...
-    thread_display_no_mq = threading.Thread(target=msg_display_no_mq, args=(data, lock_prey, lock_predator, lock_grass, flag_display,))
-    # Thread de gestion de la poussé de l'herbe
-    thread_grass = threading.Thread(target=incGrass, args=(data, lock_grass, flag_display, flag_grass,))
+    thread_display = threading.Thread(target=msg_display, args=(mq, data, lock_prey, lock_predator, lock_grass, flag_display,))  
+    # Thread de gestion de la pousse de l'herbe
+    thread_grass =  threading.Thread(target=incGrass, args = (data, lock_grass, flag_display, flag_grass))
     # Thread de gestion de secheresse avec son signal
     thread_drought = threading.Thread(target=secheresse, args=(drought_event, flag_grass, flag_display,))
     # Thread de gestion des arrivées de nouveaux animaux
     thread_connection = threading.Thread(target=gestion_connection, args=(data, server_socket, flag_display, lock_prey, lock_predator))
 
     thread_connection.start()
-    #thread_display.start()
-    thread_display_no_mq.start()
+    thread_display.start()
     thread_grass.start()
     thread_drought.start()
 
@@ -126,6 +163,7 @@ def env():
         lock_predator.release()
             
     print("flag = False")
+    stop_display(mq)
 
     ### Tue tous les process animaux
     lock_predator.acquire()
@@ -138,51 +176,14 @@ def env():
     thread_connection.join()
     thread_grass.join()
     thread_drought.join()
-    #thread_display.join()
-    thread_display_no_mq.join()
+    thread_display.join()
 
     print("every thread stop")
 
-    #stop_display(mq)
 
 
 
-### Fonction de gestion client/serveur
-def gestion_connection(data, server_socket, flag_display, lock_prey, lock_predator):
-    """
-    Fonciton qui accepte les connections de tous les nouveaux animaux et crée une thread qui gère la connection
-    Paramètres :
-        data: Permet l'accès à la shared memory et à ses fonctions
-        server_socket: socket serveur à laquelle se connecte les nouveaux animaux
-        flag_display: flag qui annonce la fin du programme
-        lock_prey: lock d'accès pour lire et modifier pop_prey
-        lock_predator: lock d'accès pour lire et modifier pop_predator
-    """
-    with server_socket:
-        server_socket.listen(1)
-        while flag_display.value :
-            readable, writable, error = select.select([server_socket], [], [], 1)
-            if server_socket in readable:
-                client_socket, address = server_socket.accept()
-                thread_client = threading.Thread(target=gestion_clients, args=(data, client_socket, address, lock_prey, lock_predator))
-                thread_client.start()
-                thread_client.join()
-                
 
-# Fonciton de gestion de la connection qui reçoit un message du nouvel animal et l'ajoute à la liste
-def gestion_clients(data, client_socket, address, lock_prey, lock_predator):
-    msg = client_socket.recv(64).decode()
-    print(msg)
-    pid, type, event = msg.split(",")
-    if event == "new" :
-        if type == "prey" :
-            lock_prey.acquire()
-            data.add_prey(pid)
-            lock_prey.release()
-        elif type == "predator" :
-            lock_predator.acquire()
-            data.add_predator(pid)
-            lock_predator.release()
         
 
 ### Fonction principale
@@ -203,11 +204,10 @@ if __name__ == "__main__":
 
     # Création de proies et de prédateurs aussi ajoutable manuelement en lançant les programmes prey.py et predator.py
     prey.main()
-    prey.main()
-    prey.main()
-    prey.main()
-    prey.main()
     predator.main()
-    predator.main()
+    display.main()
+
+    p_env.join()
+    p_memory.join()
 
 
