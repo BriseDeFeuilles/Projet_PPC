8a407c10cf7e4e6528dd94dc842d88cb17a536ed
BriseDeFeuilles <brisedefeuilles@gmail.com>
2026-01-29 10:02:02 +0100

    ajouts commentaires env.py

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#import
+### Imports
 import sys 
 from time import sleep
 #import sysv_ipc
@@ -17,7 +17,7 @@ import random
 from class_data import Data
 from Fct_mq_display_grass import stop_display, secheresse, incGrass, msg_display, msg_display_no_mq
 
-#constantes
+### Constantes de connections qui doivent être égale dans tous les programmes
 HOST = "localhost"
 PORT = 6666
 
@@ -29,12 +29,13 @@ drought_event = threading.Event()
 
 
 
-
+### Initialisation des variables de shared memory et des locks
 data = Data()
 lock_prey = mp.Lock()
 lock_predator = mp.Lock()
 lock_grass = mp.Lock()
 
+### Initialisation du manager pour la shared memory
 class MyManager(BaseManager):
     pass
 
@@ -43,7 +44,7 @@ MyManager.register("get_lock_prey", callable=lambda: lock_prey)
 MyManager.register("get_lock_predator", callable=lambda: lock_predator)
 MyManager.register("get_lock_grass", callable=lambda: lock_grass)
 
-#fonction de création de storage manager
+### Fonction de création de storage manager à appeler en process
 def shared_memory():
 
     manager = MyManager(address=("localhost", PORT_M), authkey=b"clef")
@@ -55,10 +56,11 @@ def handler_signal(signum, frame) :
     drought_event.set()
 
 
-#process principale
+### Process principale
 def env():
+    ### Connection au storage manager
     manager = MyManager(address=("localhost", PORT_M), authkey=b"clef")
-    while True:
+    while True: # Cette boucle s'assure que la connection au lieu
         try:
             manager.connect()
             break
@@ -68,27 +70,37 @@ def env():
     ### Flag utiles pour le display et l'herbe
     flag_display = Value('b', True)
     flag_grass = Value('b', True)
+
+    ### Création du signal pour la secheresse, SIGUSR1 ne fonctionne pas sur toutes les machines 
     if sys.platform != "win32":
         signal.signal(signal.SIGUSR1, handler_signal)
     else:
         print("SIGUSR1 non disponible")
 
+    ### Création de la message queue vers le display
     #mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
 
+    ### Récupération dans le process des accès aux fonction et des locks de shared memory
     data = manager.get_Data() 
     lock_prey = manager.get_lock_prey()
     lock_predator = manager.get_lock_predator()
     lock_grass = manager.get_lock_grass()
     
-
+    ### Initialisation du serveur pour la connection des process animaux
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.setblocking(False)
 
+    ### Création des threads de gestion de différents éléments et evenement
+
+    # Thread d'affichage
     #thread_display = threading.Thread(target=msg_display, args=(mq, data, lock_prey, lock_predator, lock_grass, flag_display,))      #ajouter les locks ...
     thread_display_no_mq = threading.Thread(target=msg_display_no_mq, args=(data, lock_prey, lock_predator, lock_grass, flag_display,))
+    # Thread de gestion de la poussé de l'herbe
     thread_grass = threading.Thread(target=incGrass, args=(data, lock_grass, flag_display, flag_grass,))
+    # Thread de gestion de secheresse avec son signal
     thread_drought = threading.Thread(target=secheresse, args=(drought_event, flag_grass, flag_display,))
+    # Thread de gestion des arrivées de nouveaux animaux
     thread_connection = threading.Thread(target=gestion_connection, args=(data, server_socket, flag_display, lock_prey, lock_predator))
 
     thread_connection.start()
@@ -100,8 +112,8 @@ def env():
     
     sleep(10)
 
-    ### makes sure that there are animals of both types, 
-    ### prepare to finish the simulation otherwise via flag_display
+    ### Boucle qui s'assure de la présence d'animaux des deux types
+    ### Permet de finir la simulation si non via flag_display
     while flag_display.value == True :
         sleep(3)
         lock_prey.acquire()
@@ -115,14 +127,14 @@ def env():
             
     print("flag = False")
 
-    ### kill all animal process
+    ### Tue tous les process animaux
     lock_predator.acquire()
     lock_prey.acquire()
     data.kill_all()
     lock_predator.release()
     lock_prey.release()
 
-    ### wait for all thread to finish
+    ### Attend la fin de toutes les threads
     thread_connection.join()
     thread_grass.join()
     thread_drought.join()
@@ -135,8 +147,17 @@ def env():
 
 
 
-#fonction de client/serveur
+### Fonction de gestion client/serveur
 def gestion_connection(data, server_socket, flag_display, lock_prey, lock_predator):
+    """
+    Fonciton qui accepte les connections de tous les nouveaux animaux et crée une thread qui gère la connection
+    Paramètres :
+        data: Permet l'accès à la shared memory et à ses fonctions
+        server_socket: socket serveur à laquelle se connecte les nouveaux animaux
+        flag_display: flag qui annonce la fin du programme
+        lock_prey: lock d'accès pour lire et modifier pop_prey
+        lock_predator: lock d'accès pour lire et modifier pop_predator
+    """
     with server_socket:
         server_socket.listen(1)
         while flag_display.value :
@@ -148,7 +169,7 @@ def gestion_connection(data, server_socket, flag_display, lock_prey, lock_predat
                 thread_client.join()
                 
 
-
+# Fonciton de gestion de la connection qui reçoit un message du nouvel animal et l'ajoute à la liste
 def gestion_clients(data, client_socket, address, lock_prey, lock_predator):
     msg = client_socket.recv(64).decode()
     print(msg)
@@ -164,19 +185,23 @@ def gestion_clients(data, client_socket, address, lock_prey, lock_predator):
             lock_predator.release()
         
 
-#fonction principale
+### Fonction principale
 if __name__ == "__main__":
     print("starting")
     
+    # Création du process de storage manager
     p_memory = mp.Process(target=shared_memory)
     p_memory.start()
     
     sleep(2)
 
+    # Création du process principal env
     p_env = mp.Process(target=env)
     p_env.start()
 
     sleep(2)
+
+    # Création de proies et de prédateurs aussi ajoutable manuelement en lançant les programmes prey.py et predator.py
     prey.main()
     prey.main()
     prey.main()
